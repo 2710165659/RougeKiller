a036c1e2a86ebb8eadb46d9a4f9571733b9a8054
2710165659 <2710165659@qq.com>
2025-04-02 14:10:54 +0800

    springboot项目创建

---
+++ Backend/rouge_springboot/src/main/java/com/rouge/rouge_springboot/RougeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rouge.rouge_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RougeSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RougeSpringbootApplication.class, args);
+    }
+
+}
---
+++ Backend/rouge_springboot/src/test/java/com/rouge/rouge_springboot/RougeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rouge.rouge_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RougeSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
